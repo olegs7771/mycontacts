4a98106acb8c3cf203ca1d773e5a7702e236dda8
oleg <olegs777@bezeqint.net>
2018-10-25 16:16:47 +0300

    Updates

---
+++ precache-manifest.8b603b70836c641f948067facbe560a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a005bdfaf11b4cd15462",
+    "url": "/mycontacts/static/js/runtime~main.a005bdfa.js"
+  },
+  {
+    "revision": "aa1308fd2d55f904d863",
+    "url": "/mycontacts/static/js/main.aa1308fd.chunk.js"
+  },
+  {
+    "revision": "9a2ff422b26dbcd72784",
+    "url": "/mycontacts/static/js/1.9a2ff422.chunk.js"
+  },
+  {
+    "revision": "9a2ff422b26dbcd72784",
+    "url": "/mycontacts/static/css/1.89fb4963.chunk.css"
+  },
+  {
+    "revision": "d6bf45212ad68e71ceaa4896dc6c609b",
+    "url": "/mycontacts/index.html"
+  }
+];
